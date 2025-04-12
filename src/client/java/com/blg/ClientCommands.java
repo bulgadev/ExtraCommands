1660bf06b56e5f75780ed06fddcc0d45c8f130b3
bulgadev <blinkingcape@gmail.com>
2025-04-12 18:31:02 -0300

    Command /cords added

---
+++ src/client/java/com/blg/ClientCommands.java
@@ -0,0 +1,26 @@
+package com.blg;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.text.Text;
+
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+
+public  class ClientCommands {
+    private static boolean dispcords = false;
+
+    public static boolean disp() {
+        return dispcords;
+    }
+
+    public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
+        dispatcher.register(
+                literal("cords")
+                        .executes(context -> {
+                            dispcords = !dispcords;
+                            context.getSource().getPlayer().sendMessage(Text.literal("dispcords" + dispcords), false);
+                            return 1;
+                        })
+        );
+    }
+}
