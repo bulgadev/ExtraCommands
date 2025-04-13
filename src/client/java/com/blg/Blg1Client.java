9ba5a455c4b2f6bd734e0dd8dfc8eccc60fda112
bulgadev <blinkingcape@gmail.com>
2025-04-13 00:41:36 -0300

    Added biome to /cords

--- src/client/java/com/blg/Blg1Client.java
+++ src/client/java/com/blg/Blg1Client.java
@@ -23,6 +23,13 @@ import net.minecraft.text.Text;
 import com.blg.ClientCommands; // Adjust the package path to where your ClientCommands class is located
 import static com.mojang.brigadier.arguments.StringArgumentType.getString;
 import static com.mojang.brigadier.arguments.StringArgumentType.word;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.source.BiomeAccess;
 
 //say that it is a client
 @Environment(EnvType.CLIENT)
@@ -41,13 +48,14 @@ public class Blg1Client implements ClientModInitializer {
 				ClientPlayerEntity player = mc.player;
 
 				String cords = player.getBlockPos().toString();
+				cords = cords.substring(cords.indexOf('x'), cords.indexOf('}') - 1);
 				String dim = player.getWorld().getRegistryKey().getValue().toString();
 
 				//if we in game
 				if(mc.player != null) {
 					context.drawText(mc.textRenderer, "Cordenates", 10, 130, 0xFFFFFFFF, false);
 					context.drawText(mc.textRenderer, cords, 10, 140, 0xFFFFFFFF, false);
-					context.drawText(mc.textRenderer, dim, 10, 150, 0xFFFFFFFF, false);
+					context.drawText(mc.textRenderer, userBiome(player), 10, 150, 0xFFFFFFFF, false);
 				}
 			}
 		});
@@ -57,5 +65,19 @@ public class Blg1Client implements ClientModInitializer {
 		});
 	}
 
+	private String userBiome(ClientPlayerEntity player) {
+		//cords
+		BlockPos cords = player.getBlockPos();
+		//get  player chunck
+		World world = player.getEntityWorld();
+		//biome finally
+		String bi = world.getBiome(cords).toString();
+
+		if (bi.contains(":")) {
+			bi = bi.substring(bi.indexOf(':') + 18, bi.indexOf('=') - 1);
+		}
+
+		return bi;
+	}
 
 }
